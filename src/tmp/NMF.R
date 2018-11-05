ecac3a79a12d3d76bd3d25cf93b3e2a0206782da
kur0cky <blacky.kur0cky@gmail.com>
2018-11-05 20:08:57 +0900

    fitting nmf kernel=3

--- src/tmp/NMF.R
+++ src/tmp/NMF.R
@@ -62,36 +62,60 @@ prof <- prof_data %>%
   left_join(prof_mst, by = c("qu_genre_code", "question_code", "answer_code"))
 
 
-# TF-IDF
-
+# TF-IDF ----
+# テキスト数を視聴者数で置き換え
+# 単語の頻度を視聴時間で置き換え
 # tf
 tf <- orgn2 %>% 
   group_by(house_num, program_code) %>% 
-  summarise(watch_time = sum(watch_time)) %>% # logとってもいいかも
+  summarise(watch_time = sum(watch_time)) %>% 
+  mutate(watch_time = watch_time / sum(watch_time)) %>%  # logとってもいいかも
   ungroup() 
 
 # idf
-sample %>% 
-  dbplot_bar()
-
 idf <- orgn2 %>% 
-  group_by(program_code, house_num) %>% 
+  distinct(program_code, house_num) %>% 
   count(program_code) %>% 
-  mutate(n = log(1/n))
+  mutate(idf = log(5000.0 / (n + 1.0)) ) %>% 
+  select(-n)
 
-idf <- program %>% 
-  group_by(station_code, program_code) %>% 
-  summarise(df = sum(program_time)) %>% 
-  mutate(N = sum(df)) %>% 
-  ungroup()
-  
-  
-tmp <- tf %>% 
+# tf_idf  
+tf_idf <- tf %>% 
   left_join(idf, by = "program_code") %>% 
-  transmute( house_num, program_code, tf_idf = watch_time * log(N/df + 1) ) %>% 
+  # transmute(house_num, program_code, tf_idf = watch_time * idf) %>% 
   collect()
 
-mat <- tmp %>% 
+# 
+tf <- dbGetQuery(conn,
+           "SELECT
+  program_code,
+  house_num,
+  sum(watch_time) as watch_time
+FROM
+  edit.tv_orgn_program_2
+GROUP BY
+  house_num, program_code") %>% 
+  as_tibble()
+
+idf <- dbGetQuery(conn,
+                  "SELECT DISTINCT program_code, house_num
+                  FROM
+                    edit.tv_orgn_program_2") %>% 
+  as_tibble()
+tf <- tf %>% 
+  group_by(house_num) %>% 
+  mutate(tf = watch_time / sum(watch_time)) %>% 
+  select(-watch_time) %>% 
+  ungroup()
+idf <- idf %>% 
+  count(program_code)
+tf_idf <- tf %>% 
+  left_join(idf, by = "program_code") %>% 
+  transmute(house_num, program_code, 
+            tf_idf = tf * log1p(5000 / n))
+
+
+mat <- tf_idf %>% 
   drop_na() %>% 
   spread(house_num, tf_idf, fill=0) %>% 
   as.data.frame() %>% 
@@ -100,21 +124,27 @@ mat <- tmp %>%
 
 mat <- mat[which(apply(mat,1,sum)!=0), which(apply(mat,2,sum)!=0)]
 
+library(NMF)
 res <- nmf(mat, 3)
 
 fitted(res) %>% dim
 
 basis <- basis(res)
-program <- program %>% 
-  distinct(program_code, program_name)
+pro_mst <- program %>% 
+  count(program_code, program_name) %>% 
+  collect() %>% 
+  arrange(program_code, desc(n)) %>% 
+  group_by(program_code) %>% 
+  slice(1) %>% 
+  ungroup()
 basis %>% 
   as.data.frame() %>% 
   rownames_to_column(var = "program_code") %>% 
   as_tibble() %>% 
   arrange(desc(V1, V2, V3)) %>% 
   slice(1:20) %>% 
-  select(program_code) %>% 
-  left_join(program, by = "program_code") %>% 
+  select(program_code, V1) %>% 
+  left_join(pro_mst, by = "program_code") %>% 
   select(program_code,program_name) %>% 
   head(20)
   
@@ -125,8 +155,8 @@ basis %>%
   as_tibble() %>% 
   arrange(desc(V2, V3)) %>% 
   slice(1:20) %>% 
-  select(program_code) %>% 
-  left_join(program, by = "program_code") %>% 
+  select(program_code, V2) %>% 
+  left_join(pro_mst, by = "program_code") %>% 
   select(program_code,program_name) %>% 
   head(20)
 
@@ -136,7 +166,9 @@ basis %>%
   as_tibble() %>% 
   arrange(desc(V3)) %>% 
   slice(1:20) %>% 
-  select(program_code) %>% 
-  left_join(program, by = "program_code") %>% 
+  select(program_code, V3) %>% 
+  left_join(pro_mst, by = "program_code") %>% 
   select(program_code,program_name) %>% 
   head(20)
+
+coef()
